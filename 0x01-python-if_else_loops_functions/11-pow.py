ce1fae85b81dbc30cf0839d909ee2d75719905e7
Fakhry0 <alylolo223344@gmail.com>
2024-04-20 15:02:20 +0200

    Make 11-main.py and 11-pow.py files

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def pow(a, b):
+    if b == 0:
+        return 1
+    result = a
+    for i in range(1, abs(b)):
+        result *= a
+    if b < 0:
+        return 1 / result
+    return result
