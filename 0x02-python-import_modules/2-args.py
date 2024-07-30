6fc2e2dcec9b97ed2d8f2e19a512fe53d023c95e
Fakhry0 <alylolo223344@gmail.com>
2024-07-30 22:23:50 +0300

    Add the main guard to 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 import sys
-
-if (len(sys.argv) - 1) == 0:
-    print("{} argument.".format(len(sys.argv) - 1))
-else:
-    print("{} argument:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv) - 1):
-        print("{}: {}".format(i+1, sys.argv[i+1]))
+if __name__ == "__main__":
+    if (len(sys.argv) - 1) == 0:
+        print("{} argument.".format(len(sys.argv) - 1))
+    else:
+        print("{} argument:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv) - 1):
+            print("{}: {}".format(i+1, sys.argv[i+1]))
