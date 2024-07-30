fa7da365ba5822b17ce938dae316fe9123de29a6
Fakhry0 <alylolo223344@gmail.com>
2024-07-30 22:37:46 +0300

    Make some changes and correct spellings on 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 
 if __name__ == "__main__":
     import sys
-    
+
     i = len(sys.argv) - 1
 
     if i == 0:
-        print("{} argument.".format(i))
+        print("{} arguments.".format(i))
     elif i == 1:
-        print("{} argumment:".format(i))
+        print("{} argument:".format(i))
         print("{}: {}".format(i, sys.argv[i]))
     else:
         print("{} arguments:".format(i))
