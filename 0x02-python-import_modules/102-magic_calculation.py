ce422cb743c2662423c9d812aed40c62427f84ba
Fakhry0 <alylolo223344@gmail.com>
2024-07-31 16:15:10 +0300

    Add 102-magic_calculation.py file

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,19 @@
+def magic_calculation(a, b):
+    # Importing functions from the module `magic_calculation_102`
+    from magic_calculation_102 import add, sub
+
+    # Check if `a` is less than `b`
+    if a < b:
+        # Initial calculation using the `add` function
+        c = add(a, b)
+        
+        # Loop over the range from 4 to 6 (inclusive)
+        for i in range(4, 6):
+            # Update `c` using the `add` function
+            c = add(c, i)
+        
+        # Return the final value of `c`
+        return c
+    else:
+        # If `a` is not less than `b`, use the `sub` function and return its result
+        return sub(a, b)
